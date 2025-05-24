9673b5d540135025c6553cf2d5c39d063cdd334d
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-05-24 22:25:58 +0300

    controlers, main, home

--- src/config/hbs.js
+++ src/config/hbs.js
@@ -3,6 +3,8 @@ const handlebars = require("express-handlebars");
 function configHbs(app) {
     const hbs = handlebars.create({ // създавам енджина, като специфицирам hbs като екстеншън
         extname: '.hbs'
+        /* defaultLayout: "main" ако не съм го задал като параметър експрес го приема за дефолтен main*/
+        /* по-горното важи и за partials */
     });
 
     app.engine('.hbs', hbs.engine); //регистрираме енджина в приложението върху екстеншъна hbs
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,10 @@
 const {Router} = require("express");
+const { homeController } = require("../controllers/homeController");
 
 const router = Router();
 
 //TODO add routes
+
+router.get('/', homeController)
 
 module.exports = { router }
