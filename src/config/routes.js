aeea538bb012661a63717090600f3f2c2c500dd5
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-05-25 15:41:28 +0300

    search template & link

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,5 @@
 const {Router} = require("express");
-const { home, details } = require("../controllers/catalog");
+const { home, details, search } = require("../controllers/catalog");
 const { about } = require("../controllers/about");
 const { createGet } = require("../controllers/movie");
 const { notFound } = require("../controllers/404");
@@ -12,6 +12,7 @@ router.get('/', home);
 router.get('/about', about);
 router.get('/details/:id', details);
 router.get('/create', createGet);
+router.get('/search', search);
 
 
 /* router.get('/*', notFound) */// <--Тази част от кода ми блокира изцяло приложението и всички пътища спират да работят
