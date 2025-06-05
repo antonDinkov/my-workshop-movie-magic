dc6ca588d0961ea66c5d41a23d3f4cc2e535dcc9
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-06-05 10:31:59 +0200

    edit template + get controler+ populate and validations

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,7 +4,7 @@ const { isGuest, isUser} = require('../middlewares/guards');
 
 const { home, details, search } = require("../controllers/catalog");
 const { about } = require("../controllers/about");
-const { createGet, createPost } = require("../controllers/movie");
+const { createGet, createPost, editGet, editPost } = require("../controllers/movie");
 const { createGet: createCastGet, createPost: createCastPost } = require('../controllers/cast');
 const { attachGet, attachPost } = require("../controllers/attach");
 const { registerGet, registerPost, loginGet, loginPost, logout } = require("../controllers/user");
@@ -15,14 +15,19 @@ const router = Router();
 
 router.get('/', home);
 router.get('/about', about);
+router.get('/search', search);
+
 router.get('/details/:id', details);
 router.get('/attach/:id', isUser(), attachGet);
 router.post('/attach/:id', isUser(), attachPost);
+router.get('/edit/:id', isUser(), editGet);
+router.post('/edit/:id', isUser(), editPost);
+
 router.get('/create/movie', isUser(), createGet);
 router.post('/create/movie', isUser(), createPost);
 router.get('/create/cast', isUser(), createCastGet);
 router.post('/create/cast', isUser(), createCastPost);
-router.get('/search', search);
+
 router.get('/register', isGuest(), registerGet);/* гардовете се поставят пред контролера */
 router.post('/register', isGuest(), registerPost);
 router.get('/login', isGuest(), loginGet);
--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -1,4 +1,5 @@
-const { createMovie } = require("../services/movie");
+const { createMovie, getMovieById } = require("../services/movie");
+const mongoose = require('mongoose');
 
 module.exports = {
     createGet: (req, res) => {
@@ -18,12 +19,38 @@ module.exports = {
         }
 
         if (Object.values(errors).some(e => e)) {
-            res.render('create', {movie: req.body, errors})
+            res.render('create', { movie: req.body, errors })
             return;
         }
 
         const result = await createMovie(req.body, authorId);
 
         res.redirect('/details/' + result._id);
+    },
+    editGet: async (req, res) => {
+        const movieId = req.params.id;
+
+        if (!mongoose.Types.ObjectId.isValid(movieId)) {
+            // Невалиден ID → директно 404
+            return res.status(404).render('404');
+        }
+
+        const movie = await getMovieById(movieId);
+        if (!movie) {
+            res.render('404');
+            return;
+        };
+
+        const isAuthor = req.user._id == movie.author.toString();
+
+        if (!isAuthor) {
+            res.redirect('/login');
+            return;
+        }
+
+        res.render('edit', { title: 'edit', movie });
+    },
+    editPost: (reg, res) => {
+
     }
 };
